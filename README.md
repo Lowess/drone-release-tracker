9a80db30e726d8c30042312466c1e0417dde0a6f
Florian Dambrine <florian@gumgum.com>
2023-11-10 11:29:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# :rocket: Drone-Release-Tracker
+
+Simple go program to find Drone releases (promote events) within a given time range and plot them as a heatmap.
+
+## :arrow_heading_down: Installation
+
+
+Grab the binary on the release page: [drone-release-tracker](https://github.com/Lowess/drone-release-tracker/releases/download/v1.0.0/drone-release-tracker)
+
+
+
+## :pencil2: Usage
+
+```
+./drone-release-tracker --help
+
+Usage of ./drone-release-tracker:
+  -from string
+        Releases after this date will be included (default "2023-10-01")
+  -output string
+        Output format (json, png, jpeg, gif, svg) (default "png")
+  -repo string
+        Name of the repository to list releases for (default "octocat/demo")
+  -to string
+        Releases before this date will be included (default "2023-12-31")
+```
+
+```sh
+# Grab your personal credentials from your Drone server
+export DRONE_SERVER=https://drone.company.org
+export DRONE_TOKEN=...
+
+./drone-release-tracker --from 2023-04-01 --to 2023-11-10 --repo octocat/demo --output png > chart.png
+```
+
+---
+
+## :pray: Credits
+
+Big thanks to [@nikolaydubina](https://github.com/nikolaydubina) for all his great work on [calendarheatmap](https://calendarheatmap.io/) which is used in this project
---
+++ drone-release-tracker.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"image/color"
+	"log"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/drone/drone-go/drone"
+	"golang.org/x/oauth2"
+
+	"github.com/nikolaydubina/calendarheatmap/charts"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+)
+
+var (
+	fromFlag   = flag.String("from", getQuarterStartDate(time.Now()).Format("2006-01-02"), "Releases after this date will be included")
+	toFlag     = flag.String("to", getQuarterEndDate(time.Now()).Format("2006-01-02"), "Releases before this date will be included")
+	repoFlag   = flag.String("repo", "octocat/demo", "Name of the repository to list releases for")
+	outputFlag = flag.String("output", "png", "Output format (json, png, jpeg, gif, svg)")
+)
+
+var (
+	token = os.Getenv("DRONE_TOKEN")
+	host  = os.Getenv("DRONE_SERVER")
+)
+
+//go:embed assets/fonts/Sunflower-Medium.ttf
+var defaultFontFaceBytes []byte
+
+//go:embed assets/colorscales/green-blue-9.csv
+var defaultColorScaleBytes []byte
+
+// Return a list of builds that started after a given date.
+func filterReleasesByDate(builds []*drone.Build, from time.Time, to time.Time) []*drone.Build {
+	filteredBuilds := make([]*drone.Build, 0)
+
+	for _, build := range builds {
+		// Only Include releases (eg. promoted builds
+		if build.Event == "promote" && build.Deploy == "production" {
+			buildTime := time.Unix(build.Created, 0)
+			if buildTime.After(from) && buildTime.Before(to) {
+				// Include the build if its creation date is after the given date.
+				filteredBuilds = append(filteredBuilds, build)
+			}
+		}
+	}
+
+	return filteredBuilds
+}
+
+// Find all releases until a given date
+func findAllReleasesWithinRange(client drone.Client, namespace string, repo string, from time.Time, to time.Time, page int) []*drone.Build {
+
+	paginatedBuilds, err := client.BuildList(namespace, repo, drone.ListOptions{Page: page, Size: 25})
+	if err != nil || len(paginatedBuilds) == 0 {
+		return nil
+	}
+
+	// Recursion end condition: First build item fetched from paginated response is older than until date
+	oldestBuild := paginatedBuilds[0]
+	oldestBuildTime := time.Unix(oldestBuild.Created, 0)
+
+	if oldestBuildTime.Before(from) {
+		return nil
+	}
+
+	filteredReleases := filterReleasesByDate(paginatedBuilds, from, to)
+	nextReleases := findAllReleasesWithinRange(client, namespace, repo, from, to, page+1)
+	return append(filteredReleases, nextReleases...)
+}
+
+func getQuarterStartDate(currentDate time.Time) time.Time {
+	year, month, _ := currentDate.Date()
+	quarterStartMonth := time.Month(((int(month)-1)/3)*3 + 1)
+	return time.Date(year, quarterStartMonth, 1, 0, 0, 0, 0, currentDate.Location())
+}
+
+func getQuarterEndDate(currentDate time.Time) time.Time {
+	year, month, _ := currentDate.Date()
+	quarterStartMonth := time.Month(((int(month)-1)/3)*3 + 1)
+	quarterEndMonth := quarterStartMonth + 2 // The end month is three months after the start month.
+	quarterEndMonth %= 12                    // Ensure the month doesn't exceed 12 (December).
+	if quarterEndMonth < quarterStartMonth {
+		year++
+	}
+	lastDayOfQuarter := time.Date(year, quarterEndMonth+1, 0, 0, 0, 0, 0, currentDate.Location())
+	return lastDayOfQuarter
+}
+
+// Taken from https://github.com/nikolaydubina/calendarheatmap/blob/master/main.go.
+func plotHeatmap(data []byte, outputFormat string) {
+
+	labels := true
+	monthSep := true
+	colorScale := "green-blue-9.csv"
+	locale := "en_US"
+
+	var colorscale charts.BasicColorScale
+	if assetsPath := os.Getenv("CALENDAR_HEATMAP_ASSETS_PATH"); assetsPath != "" {
+		var err error
+		colorscale, err = charts.NewBasicColorscaleFromCSVFile(path.Join(assetsPath, "colorscales", colorScale))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		var err error
+		if colorScale != "green-blue-9.csv" {
+			log.Printf("defaulting to colorscale %s since CALENDAR_HEATMAP_ASSETS_PATH is not set", "green-blue-9.csv")
+		}
+		colorscale, err = charts.NewBasicColorscaleFromCSV(bytes.NewBuffer(defaultColorScaleBytes))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fontFace, err := charts.LoadFontFace(defaultFontFaceBytes, opentype.FaceOptions{
+		Size:    26,
+		DPI:     280,
+		Hinting: font.HintingNone,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var counts map[string]int
+	if err := json.Unmarshal(data, &counts); err != nil {
+		log.Fatal(err)
+	}
+	conf := charts.HeatmapConfig{
+		Counts:              counts,
+		ColorScale:          colorscale,
+		DrawMonthSeparator:  monthSep,
+		DrawLabels:          labels,
+		Margin:              30,
+		BoxSize:             150,
+		MonthSeparatorWidth: 5,
+		MonthLabelYOffset:   50,
+		TextWidthLeft:       300,
+		TextHeightTop:       200,
+		TextColor:           color.RGBA{100, 100, 100, 255},
+		BorderColor:         color.RGBA{200, 200, 200, 255},
+		Locale:              locale,
+		Format:              outputFormat,
+		FontFace:            fontFace,
+		ShowWeekdays: map[time.Weekday]bool{
+			time.Monday:    true,
+			time.Wednesday: true,
+			time.Friday:    true,
+		},
+	}
+	charts.WriteHeatmap(conf, os.Stdout)
+}
+
+type DroneRepo string
+
+func (r DroneRepo) Split(str string) (string, string, error) {
+	s := strings.Split(string(r), str)
+	if len(s) < 2 {
+		return "", "", errors.New("Drone repository should be made of <namespace>/<name>")
+	}
+	return s[0], s[1], nil
+}
+
+func main() {
+	// Parse flags
+	flag.Parse()
+
+	// create an http client with oauth authentication.
+	config := new(oauth2.Config)
+	author := config.Client(
+		context.Background(),
+		&oauth2.Token{
+			AccessToken: token,
+		},
+	)
+
+	// create the drone client with authenticator
+	client := drone.NewClient(host, author)
+
+	// find all releases within date range
+	fromDate, _ := time.Parse("2006-01-02", *fromFlag)
+	toDate, _ := time.Parse("2006-01-02", *toFlag)
+
+	droneRepo := DroneRepo(*repoFlag)
+
+	namespace, name, err := droneRepo.Split("/")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	releases := findAllReleasesWithinRange(client, namespace, name, fromDate, toDate, 1)
+
+	heatmapData := make(map[string]int)
+	for _, build := range releases {
+		releaseDate := time.Unix(build.Created, 0)
+		heatmapData[releaseDate.Format("2006-01-02")] += 1
+	}
+
+	heatmapDataJson, _ := json.MarshalIndent(heatmapData, "", "")
+
+	if *outputFlag == "json" {
+		fmt.Println(string(heatmapDataJson))
+	} else {
+		// Plot heatmap
+		plotHeatmap(heatmapDataJson, *outputFlag)
+	}
+
+}
